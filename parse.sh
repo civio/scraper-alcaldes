990dceb971a78224f20b808fd040afb97e1d142b
David Cabo <david.cabo@gmail.com>
2018-05-18 19:38:05 +0200

    Add header to output file

--- parse.sh
+++ parse.sh
@@ -1,6 +1,6 @@
 mkdir output 2> /dev/null
 
-rm output/alcaldes.csv 2> /dev/null
+echo "provincia,municipio,nombre,apellido 1,apellido 2,fecha posesión,lista" > output/alcaldes.csv
 
 for (( i=1; i<=52; i++ ))
 do
